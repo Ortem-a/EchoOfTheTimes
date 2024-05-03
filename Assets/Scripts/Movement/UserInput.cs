bb152b4764901e18bed855a2049a58d0531c2607
pmeshcheryakov <70879420+pmescheriakov@users.noreply.github.com>
2024-05-03 17:55:49 +0300

    ы

--- Assets/Scripts/Movement/UserInput.cs
+++ Assets/Scripts/Movement/UserInput.cs
@@ -37,7 +37,10 @@ namespace EchoOfTheTimes.Movement
             _camera = Camera.main;
         }
 
-        private void Update() // ляляля тут и свайп детектится и тач, и нет тача по вертексу если свайп начинать с точки вертекса, и дабл тач по пустому месту
+
+
+        // никаких свайпов и дабл тачей, я тут главный и лучше знаю как камера должна быть, игрок ничто, игра всё
+        private void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -53,7 +56,7 @@ namespace EchoOfTheTimes.Movement
                 float touchDuration = Time.time - _touchStartTime;
                 float touchDistance = Vector2.Distance(_startSwipePosition, Input.mousePosition);
 
-                if (touchDuration <= MaxTapTime && touchDistance < MinSwipeDistance / 2 && !_swipeActivated)
+                if (touchDuration <= MaxTapTime)
                 {
                     Vector3 touchPosition3D = Camera.main.ScreenToWorldPoint(new Vector3(_startSwipePosition.x, _startSwipePosition.y, Camera.main.nearClipPlane));
                     RaycastHit hit;
@@ -65,51 +68,85 @@ namespace EchoOfTheTimes.Movement
                             _userInputHandler.OnTouched?.Invoke(vertex);
                             _isSuccessfulTap = true;
                         }
-                    }
-
-                    if (!_isSuccessfulTap && Time.time - _lastTapTime < DoubleTapDelta)
-                    {
-                        _userInputHandler.OnDoubleTouched?.Invoke(); // Вырезать даблтач, заменив на тач по кнопке для возврата
-                        _lastTapTime = 0f;
-                    }
-                    else
-                    {
-                        _lastTapTime = Time.time;
-                    }
-                }
-
-                _isSwiping = false;
-                _swipeActivated = false;
-            }
-
-            if (Input.GetMouseButton(0) && _isSwiping) // предполагаем что любое касание = начало свайпа
-            {
-                Vector2 currentSwipePosition = Input.mousePosition;
-
-                // Считали активированный каким-то чудом свайп
-                if (!_swipeActivated && (Vector2.Distance(_startSwipePosition, currentSwipePosition) > MinSwipeDistance || Time.time - _touchStartTime > MaxTapTime))
-                {
-                    _swipeActivated = true;
-                    // _startSwipePosition = currentSwipePosition;
-                }
-
-                if (_swipeActivated)
-                {
-
-                    // Свайп в какой величине?
-                    float deltaX = currentSwipePosition.x - _startSwipePosition.x;
-                    // Получаем ширину экрана в дюймах
-                    float screenWidthInches = Screen.width / Screen.dpi;
-                    // Преобразуем deltaX в дюймы
-                    float deltaXInches = deltaX / Screen.dpi;
-                    // Вычисляем угол поворота, так чтобы deltaXInches, равный ширине экрана в дюймах, эквивалентен 180 градусам
-                    float rotationAngle = (deltaXInches / screenWidthInches) * 180;
-
-                    _userInputHandler.OnSwiped?.Invoke(rotationAngle);
-
-                    _startSwipePosition = currentSwipePosition; // Я/МЫ еблан? Я считываю микро-отрезочки каждый кадр вместо считывания фул свайпа, и как мне это овкуснивать
+                    }   
                 }
             }
         }
+
+        // ляляля тут и свайп детектится и тач, и нет тача по вертексу если свайп начинать с точки вертекса, и дабл тач по пустому месту
+        //private void Update() 
+        //{
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        _startSwipePosition = Input.mousePosition;
+        //        _touchStartTime = Time.time;
+        //        _isSwiping = true;
+        //        _swipeActivated = false;
+        //        _isSuccessfulTap = false;
+        //    }
+
+        //    if (Input.GetMouseButtonUp(0))
+        //    {
+        //        float touchDuration = Time.time - _touchStartTime;
+        //        float touchDistance = Vector2.Distance(_startSwipePosition, Input.mousePosition);
+
+        //        if (touchDuration <= MaxTapTime && touchDistance < MinSwipeDistance / 2 && !_swipeActivated)
+        //        {
+        //            Vector3 touchPosition3D = Camera.main.ScreenToWorldPoint(new Vector3(_startSwipePosition.x, _startSwipePosition.y, Camera.main.nearClipPlane));
+        //            RaycastHit hit;
+        //            if (Physics.Raycast(touchPosition3D, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        //            {
+        //                EchoOfTheTimes.Core.Vertex vertex = hit.transform.GetComponent<EchoOfTheTimes.Core.Vertex>();
+        //                if (vertex != null)
+        //                {
+        //                    _userInputHandler.OnTouched?.Invoke(vertex);
+        //                    _isSuccessfulTap = true;
+        //                }
+        //            }
+
+        //            if (!_isSuccessfulTap && Time.time - _lastTapTime < DoubleTapDelta)
+        //            {
+        //                _userInputHandler.OnDoubleTouched?.Invoke(); // Вырезать даблтач, заменив на тач по кнопке для возврата
+        //                _lastTapTime = 0f;
+        //            }
+        //            else
+        //            {
+        //                _lastTapTime = Time.time;
+        //            }
+        //        }
+
+        //        _isSwiping = false;
+        //        _swipeActivated = false;
+        //    }
+
+        //    if (Input.GetMouseButton(0) && _isSwiping) // предполагаем что любое касание = начало свайпа
+        //    {
+        //        Vector2 currentSwipePosition = Input.mousePosition;
+
+        //        // Считали активированный каким-то чудом свайп
+        //        if (!_swipeActivated && (Vector2.Distance(_startSwipePosition, currentSwipePosition) > MinSwipeDistance || Time.time - _touchStartTime > MaxTapTime))
+        //        {
+        //            _swipeActivated = true;
+        //            // _startSwipePosition = currentSwipePosition;
+        //        }
+
+        //        if (_swipeActivated)
+        //        {
+
+        //            // Свайп в какой величине?
+        //            float deltaX = currentSwipePosition.x - _startSwipePosition.x;
+        //            // Получаем ширину экрана в дюймах
+        //            float screenWidthInches = Screen.width / Screen.dpi;
+        //            // Преобразуем deltaX в дюймы
+        //            float deltaXInches = deltaX / Screen.dpi;
+        //            // Вычисляем угол поворота, так чтобы deltaXInches, равный ширине экрана в дюймах, эквивалентен 180 градусам
+        //            float rotationAngle = (deltaXInches / screenWidthInches) * 180;
+
+        //            _userInputHandler.OnSwiped?.Invoke(rotationAngle);
+
+        //            _startSwipePosition = currentSwipePosition; // Я/МЫ еблан? Я считываю микро-отрезочки каждый кадр вместо считывания фул свайпа, и как мне это овкуснивать
+        //        }
+        //    }
+        //}
     }
 }
