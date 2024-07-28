cf5a0a60bcd9090de541cc09ab38c7d088be9577
pmeshcheryakov <70879420+pmescheriakov@users.noreply.github.com>
2024-07-28 22:24:58 +0300

    Жду чуда - 25/17
    чудо если я не умру этой ночью

--- Assets/Scripts/Effects/LevelAudioManager.cs
+++ Assets/Scripts/Effects/LevelAudioManager.cs
@@ -1,9 +1,9 @@
 using DG.Tweening;
 using EchoOfTheTimes.ScriptableObjects.Level;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
+using EchoOfTheTimes.Units; // Добавьте пространство имен, в котором находится класс Player
 
 namespace EchoOfTheTimes.Effects
 {
@@ -13,6 +13,14 @@ namespace EchoOfTheTimes.Effects
         [SerializeField]
         private LevelSoundsSceneContainerScriptableObject _levelSoundsSceneContainer;
         private bool _isReadyToPlaySound = false; // Флаг для отслеживания готовности к воспроизведению звука
+        private AudioSource audioSource;
+        private Player player;
+
+        [Inject]
+        private void Construct(Player playerInstance)
+        {
+            player = playerInstance;
+        }
 
         private void Start()
         {
@@ -29,7 +37,7 @@ namespace EchoOfTheTimes.Effects
                 if (levelSound != null)
                 {
                     Debug.Log($"Playing sound for scene: {currentSceneName}"); // Отладка
-                    AudioSource.PlayClipAtPoint(levelSound.ChangeStateSound, Vector3.zero); // Убедитесь, что у вас есть подходящий источник воспроизведения
+                    PlaySound(levelSound.ChangeStateSound, 1.0f); // Задаем громкость
                 }
                 else
                 {
@@ -40,6 +48,23 @@ namespace EchoOfTheTimes.Effects
             {
                 Debug.LogWarning($"Sound not ready to play for scene: {SceneManager.GetActiveScene().name}"); // Отладка
             }
+        }
+
+        private void PlaySound(AudioClip clip, float volume)
+        {
+            // Создаем временный объект как дочерний объект игрока
+            GameObject tempAudioGO = new GameObject("TempAudio");
+            tempAudioGO.transform.SetParent(player.transform); // Устанавливаем родительский объект
+            tempAudioGO.transform.localPosition = Vector3.zero; // Устанавливаем локальную позицию
+
+            audioSource = tempAudioGO.AddComponent<AudioSource>(); // Добавляем компонент AudioSource
+            audioSource.clip = clip; // Устанавливаем аудиоклип
+            audioSource.volume = volume; // Устанавливаем громкость
+            audioSource.spatialBlend = 1.0f; // Устанавливаем пространственный звук
+            audioSource.Play(); // Воспроизводим звук
+
+            // Уничтожаем временный объект после завершения воспроизведения
+            Destroy(tempAudioGO, clip.length);
         }
 
         private LevelSoundsSceneContainerScriptableObject.LevelSound GetLevelSound(string sceneName)
