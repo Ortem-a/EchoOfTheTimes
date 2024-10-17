1bab68ce240004dc18a7801a84dfb0916ecb5357
pmeshcheryakov <70879420+pmescheriakov@users.noreply.github.com>
2024-10-17 20:18:54 +0300

    я щас в окно выйду

--- Assets/Scripts/SceneManagement/SceneLoader.cs
+++ Assets/Scripts/SceneManagement/SceneLoader.cs
@@ -189,10 +189,11 @@ namespace EchoOfTheTimes.SceneManagement
             });
 
             AppMetrica.ReportEvent("level_completed", jsonData);
+            ;
 
             if (AppMetrica.IsActivated()) 
             {
-                Debug.Log("AppMetrica АКТИВНА");
+                Debug.Log(AppMetrica.GetDeviceId());
             }
 
             Debug.Log("Отправили длительность уровня");
