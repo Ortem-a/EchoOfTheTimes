b290a73e77030f7f69215c0aad430d2bbc449da4
pmeshcheryakov <70879420+pmescheriakov@users.noreply.github.com>
2024-10-28 05:14:56 +0300

    Поиграл с шарами

--- Assets/Scripts/UI/MainMenu/ChapterButtonView.cs
+++ Assets/Scripts/UI/MainMenu/ChapterButtonView.cs
@@ -10,6 +10,7 @@ namespace EchoOfTheTimes.UI.MainMenu
 
         [SerializeField] private GameObject additionalObjectToDisable;
         [SerializeField] private GameObject anotherObjectToGrayOut;
+        [SerializeField] private Image directObjectToGrayOut;
 
         private void Awake()
         {
@@ -24,15 +25,18 @@ namespace EchoOfTheTimes.UI.MainMenu
                     _childImage.color = Color.gray;
                     additionalObjectToDisable.transform.GetChild(0).gameObject.SetActive(false);
                     anotherObjectToGrayOut.transform.GetChild(0).GetComponent<Image>().color = Color.gray;
+                    directObjectToGrayOut.color = Color.gray;
                     break;
                 case StatusType.Unlocked:
                     _childImage.color = Color.white;
                     additionalObjectToDisable.transform.GetChild(0).gameObject.SetActive(true);
                     anotherObjectToGrayOut.transform.GetChild(0).GetComponent<Image>().color = Color.white;
+                    directObjectToGrayOut.color = Color.white;
                     break;
                 case StatusType.Completed:
                     additionalObjectToDisable.transform.GetChild(0).gameObject.SetActive(true);
                     anotherObjectToGrayOut.transform.GetChild(0).GetComponent<Image>().color = Color.white;
+                    directObjectToGrayOut.color = new Color(0.7f, 1f, 0.7f);
                     break;
             }
         }
