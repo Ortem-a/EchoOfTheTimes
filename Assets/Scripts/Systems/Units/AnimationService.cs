8263f6389947c108376aa8a0e573ec486817985c
Artyom <60854544+Ortem-a@users.noreply.github.com>
2025-02-17 23:40:36 +0300

    Update AnimationService.cs

--- Assets/Scripts/Systems/Units/AnimationService.cs
+++ Assets/Scripts/Systems/Units/AnimationService.cs
@@ -8,14 +8,14 @@ namespace Systems.Units
         private readonly Animator _animator;
         private readonly Dictionary<int, float> _animationsDuration;
 
-        private const float _crossFadeDuration = 0.1f;
+        private const float _crossFadeDuration = 0.01f;
 
         private readonly int _idleHash = Animator.StringToHash("Idle");
         private readonly int _moveHash = Animator.StringToHash("Walking");
         private readonly int _climbingHash = Animator.StringToHash("Climbing Ladder");
         private readonly int _finishClimbingHash = Animator.StringToHash("Finish Climbing");
         private readonly int _startClimbingHash = Animator.StringToHash("Start Climbing");
-        private readonly int _runnigUpHash = Animator.StringToHash("Running Up Stairs");
+        private readonly int _runningUpHash = Animator.StringToHash("Running Up Stairs");
         private readonly int _walkingUpHash = Animator.StringToHash("Walking UP");
 
         public AnimationService(Animator animator)
@@ -29,7 +29,7 @@ namespace Systems.Units
                 { _climbingHash, 0.1f },
                 { _finishClimbingHash, 0.1f },
                 { _startClimbingHash, 0.1f },
-                { _runnigUpHash, 0.1f },
+                { _runningUpHash, 0.1f },
                 { _walkingUpHash, 0.1f },
             };
         }
@@ -39,13 +39,12 @@ namespace Systems.Units
         public float Climb() => PlayAnimation(_climbingHash);
         public float FinishClimb() => PlayAnimation(_finishClimbingHash);
         public float StartClimb() => PlayAnimation(_startClimbingHash);
-        public float RunUp() => PlayAnimation(_runnigUpHash);
+        public float RunUp() => PlayAnimation(_runningUpHash);
         public float WalkUp() => PlayAnimation(_walkingUpHash);
 
         private float PlayAnimation(int animationHash)
         {
             _animator.CrossFade(animationHash, _crossFadeDuration);
-            _animator.Play(animationHash);
             return _animationsDuration[animationHash];
         }
     }
