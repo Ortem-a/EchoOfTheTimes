57697f524fe455c88163c4d27cf53d32a1959b1d
pmeshcheryakov <70879420+pmescheriakov@users.noreply.github.com>
2025-03-18 00:25:53 +0300

    Пупупу
    Проверить как двигается перс на 1.1 (АРТЁМ)

--- Assets/Scripts/Systems/UI/MainMenu/TestUiButtonController.cs
+++ Assets/Scripts/Systems/UI/MainMenu/TestUiButtonController.cs
@@ -17,6 +17,20 @@ namespace Systems.UI.MainMenu
 
             Buttons[0].onClick.AddListener(
                 async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[0]));
+            Buttons[1].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[1]));
+            Buttons[2].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[2]));
+            Buttons[3].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[3]));
+            Buttons[4].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[4]));
+            Buttons[5].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[5]));
+            Buttons[6].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[6]));
+            Buttons[7].onClick.AddListener(
+                async () => await _loader.LoadLevelAsync(_loader.Chapters[1].Levels[7]));
         }
     }
 }
